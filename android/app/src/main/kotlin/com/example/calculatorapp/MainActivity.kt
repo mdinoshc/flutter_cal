e7dbd8c9114a7c39844ff0e9a2002aea19e153f0
chritonarescomp <chriton@arescomp.com>
2025-06-14 11:09:00 +0530

    Call App - app versioning and other fixes

--- android/app/src/main/kotlin/com/example/calculatorapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calculatorapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculatorapp
 
 import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
---
+++ lib/keys/keyz.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class testKeyz {
+  static GlobalKey<State<StatefulWidget>> firstNumKey = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> secondNumKey = GlobalKey();
+
+  static GlobalKey<State<StatefulWidget>> firstNumKeyQec = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> secondNumKeyQec = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> thirdNumKeyQec = GlobalKey();
+
+  static GlobalKey<State<StatefulWidget>> AddBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> SubBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> MultiplyBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> DivideBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> ModBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> PowBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> RootBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> ClrBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> QECBtn = GlobalKey();
+
+  static GlobalKey<State<StatefulWidget>> CalBtn = GlobalKey();
+  static GlobalKey<State<StatefulWidget>> ClrQecBtn = GlobalKey();
+}
